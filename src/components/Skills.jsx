3458aa998097414d5af76deaed1b349824093049
Catalin Mihai <75864270+qcatatv@users.noreply.github.com>
2024-10-04 00:01:07 +0300

    Add files via upload

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,14 +4,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Skills() {
   return (
-    <div className='flex justify-center content-center gap-5'>
-        <img className='' width="144" height="144" src="https://img.icons8.com/color/480/html-5--v1.png" alt="html-5--v1"/>
-        <img width="144" height="144" src="https://img.icons8.com/color/144/css3.png" alt="css3"/>
-        <img width="144" height="144" src="https://img.icons8.com/color/144/angularjs.png" alt="angularjs"/>
-        <img width="144" height="144" src="https://img.icons8.com/color/144/react-native.png" alt="react-native"/>
-        <img width="144" height="144" src="https://img.icons8.com/fluency/144/tailwind_css.png" alt="tailwind_css"/>
-        <img width="144" height="144" src="https://img.icons8.com/fluency/144/bootstrap.png" alt="bootstrap"/>
-        <img width="144" height="144" src="https://img.icons8.com/color/144/figma--v1.png" alt="figma--v1"/>
+    <div className='flex flex-row items-center justify-center content-center gap-5 mt-5'>
+        <img className='object-contain h-20 w-auto ' width="144" height="144" src="https://img.icons8.com/color/480/html-5--v1.png" alt="html-5--v1"/>
+        <img className='object-contain h-20 w-auto' width="144" height="144" src="https://img.icons8.com/color/144/css3.png" alt="css3"/>
+        <img className='object-contain h-20 w-auto' width="144" height="144" src="https://img.icons8.com/color/144/angularjs.png" alt="angularjs"/>
+        <img className='object-contain h-20 w-auto' width="144" height="144" src="https://img.icons8.com/color/144/react-native.png" alt="react-native"/>
+        <img className='object-contain h-20 w-auto' width="144" height="144" src="https://img.icons8.com/fluency/144/tailwind_css.png" alt="tailwind_css"/>
+        <img className='object-contain h-20 w-auto' width="144" height="144" src="https://img.icons8.com/fluency/144/bootstrap.png" alt="bootstrap"/>
+        <img className='object-contain h-20 w-auto' width="144" height="144" src="https://img.icons8.com/color/144/figma--v1.png" alt="figma--v1"/>
     </div>
   )
 }
