b470e9d2f73ced021caf4214144a6c68d078b181
Catalin Mihai <75864270+qcatatv@users.noreply.github.com>
2024-10-04 07:02:47 +0300

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,6 +6,7 @@ function Skills() {
   return (
     <div className='flex flex-row items-center justify-center content-center gap-5 mt-10'>
       <img className='object-contain h-20 w-auto' width="144" height="144" src="https://img.icons8.com/color/144/react-native.png" alt="react-native"/>
+      <img className='object-contain h-20 w-auto' width="144" height="144" src="https://img.icons8.com/color/144/html-5--v1.png" alt="html-5--v1"/>
       <img className='object-contain h-20 w-auto' width="144" height="144" src="https://img.icons8.com/color/144/css3.png" alt="css3"/>
       <img className='object-contain h-20 w-auto' width="144" height="144" src="https://img.icons8.com/fluency/144/tailwind_css.png" alt="tailwind_css"/>
       <img className='object-contain h-20 w-auto' width="144" height="144" src="https://img.icons8.com/fluency/144/bootstrap.png" alt="bootstrap"/>
