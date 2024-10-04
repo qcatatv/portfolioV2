e8cf086175a941251857824c9ac9b6a0c475cafe
Catalin Mihai <75864270+qcatatv@users.noreply.github.com>
2024-10-04 05:49:57 +0300

    Add files via upload

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
 
 function Contact() {
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "0f71ad41-4514-487b-92f4-e1e2baa5ed53");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        try {
+            const res = await fetch("https://api.web3forms.com/submit", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json"
+                },
+                body: json
+            });
+    
+            const data = await res.json();
+    
+            if (res.ok) {
+                console.log("Success", data);
+            } else {
+                console.error("Error: ", data.message || "Submission failed");
+                console.error("Response Status: ", res.status);
+            }
+        } catch (error) {
+            console.error("Error submitting form:", error);
+        }
+    };
+
   return (
     <div>
-        <section className="bg-white bg-transparent">
+        <section className=" bg-transparent">
             <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
                 <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
                 <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
-                <form action="#" class="space-y-8">
+                <form onSubmit={onSubmit} class="space-y-8">
                     <div>
                         <label htmlFor="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
                         <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required></input>
